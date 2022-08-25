8ad97272b7e6469fbe21250e3ecc666c8d70068d
babols <julien.delpeut@laposte.net>
2022-08-25 10:46:41 +0200

    challenge 04.bis

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 
+const { validateMovie } = require("./validators.js");
+const { validateUser } = require("./validators.js");
+
 const app = express();
 
 app.use(express.json());
@@ -12,10 +15,15 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers")
+
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, userHandlers.postUser);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
@@ -23,25 +31,8 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", userHandlers.postUser);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", userHandlers.deleteUser);
