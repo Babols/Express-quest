d224843ce1b315bce570cf2f5f484bf649a209cf
babols <julien.delpeut@laposte.net>
2022-09-13 14:30:53 +0200

    finish 07

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 
-const { validateMovie } = require("./validators.js");
-const { validateUser } = require("./validators.js");
-
 const app = express();
 
 app.use(express.json());
@@ -15,26 +12,25 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
-
 app.get("/", welcome);
 
+const { validateMovie } = require("./validators.js");
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers")
-
-
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", validateUser, userHandlers.postUser);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+const { validateUser } = require("./validators");
+const userHandlers = require("./userHandlers");
+const { hashPassword } = require("./auth");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.put("/api/users/:id", validateUser, userHandlers.updateUser);
-
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", validateUser, hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
